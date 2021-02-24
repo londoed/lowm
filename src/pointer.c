4c99e94c5e3aba72d1efccdf36d23ec4ccbe4709
Eric <londoed@comcast.net>
2021-02-24 03:32:27 -0500

    Done for the night

---
+++ src/pointer.c
@@ -0,0 +1,366 @@
+/**
+ * Copyright (C) 2021, Eric Londo <londoed@comcast.net>, { src/pointer.c }
+ * This software is distributed under the GNU General Public License Version 2.0.
+ * See the file LICENSE for details.
+**/
+#include <xcb/xcb_keysyms.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "lowm.h"
+#include "query.h"
+#include "settings.h"
+#include "stack.h"
+#include "tree.h"
+#include "monitor.h"
+#include "subscribe.h"
+#include "events.h"
+#include "window.h"
+#include "pointer.h"
+
+uint16_t num_lock;
+uint16_t caps_lock;
+uint16_t scroll_lock;
+bool grabbing;
+node_t *grabbed_node;
+
+void
+pointer_init(void)
+{
+    num_lock = modfield_from_keysym(XK_Num_Lock);
+    caps_lock = modfield_from_keysym(XK_Caps_Lock);
+    scroll_lock = modfield_from_keysym(XK_Scroll_Lock);
+
+    if (caps_lock == XCB_NO_SYMBOL)
+        caps_lock = XCB_MOD_MASK_LOCK;
+
+    grabbing = false;
+    grabbed_node = NULL;
+}
+
+void
+window_grab_buttons(xcb_window_t win)
+{
+    unsigned int i;
+
+    for (i = 0; i < LENGTH(BUTTONS); i++) {
+        if (click_to_focus == (int8_t)XCB_BUTTON_INDEX_ANY || click_to_focus == (int8_t)BUTTONS[i])
+            window_grab_buttons(win, BUTTONS[i], XCB_NONE);
+
+        if (pointer_actions[i] != ACTION_NODE)
+            window_grab_buttons(win, BUTTONS[i], pointer_modifier);
+    }
+}
+
+void
+window_grab_buttons(xcb_window_t win, uint8_t button, uint16_t modifier)
+{
+#define GRAB(b, m)                                                          \
+    xcb_grab_button(dpy, false, win, XCB_EVENT_MASK_BUTTON_PRESS,           \
+        XCB_GRAB_MODE_SYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, b, m)
+
+    GRAB(button, modifier);
+
+    if (num_lock != XCB_NO_SYMBOL && caps_lock != XCB_NO_SYMBOL && scroll_lock != XCB_NO_SYMBOL)
+        GRAB(button, modifier | num_lock | caps_lock | scroll_lock);
+
+    if (num_lock != XCB_NO_SYMBOL && caps_lock != XCB_NO_SYMBOL)
+        GRAB(button, modifier | num_lock | caps_lock);
+
+    if (caps_lock != XCB_NO_SYMBOL && scroll_lock != XCB_NO_SYMBOL)
+        GRAB(button, modifier | caps_lock | scroll_lock);
+
+    if (num_locks != XCB_NO_SYMBOL && scroll_lock != XCB_NO_SYMBOL)
+        GRAB(button, modifier | num_lock | scroll_lock);
+
+    if (num_lock != XCB_NO_SYMBOL)
+        GRAB(button, modifier | num_lock);
+
+    if (caps_lock != XCB_NO_SYMBOL)
+        GRAB(button, modifier | caps_lock);
+
+    if (scroll_lock != XCB_NO_SYMBOL)
+        GRAB(button, modifier | scroll_lock);
+#undef GRAB
+}
+
+void
+grab_buttons(void)
+{
+    monitor_t *m;
+    desktop_t *d;
+    node_t *n;
+
+    for (*m = mon_head; m != NULL; m = m->next) {
+        for (*d = m->desk_head; d != NULL; d = d->next) {
+            for (*n = first_extrema(d->root); n != NULL; n = next_leaf(n, d->root)) {
+                window_grab_buttons(n->id);
+
+                if (n->presel != NULL)
+                    window_grab_buttons(n->presel->feedback);
+            }
+        }
+    }
+}
+
+void
+ungrab_buttons(void)
+{
+    monitor_t *m;
+    desktop_t *d;
+    node_t *n;
+
+    for (*m = mon_head; m != NULL; m = m->next) {
+        for (*d = m->desk_head; d != NULL; d = d->next) {
+            for (*n = first_extrema(d->root); n != NULL; n = next_leaf(n, d->root)) {
+                window_grab_buttons(n->id);
+
+                if (n->presel != NULL)
+                    window_grab_buttons(n->presel->feedback);
+            }
+        }
+    }
+}
+
+int16_t
+modfield_from_keysym(xcb_keysym_t keysym)
+{
+    uint16_t modfield = 0;
+    xcb_keycode_t *keycode = NULL, *mod_keycodes = NULL;
+    xcb_get_modifier_mapping_reply_t *reply = NULL;
+    xcb_key_symbol_t *symbols = xcb_key_symbols_alloc(dpy);
+
+    if ((keycodes = xcb_key_symbols_get_keycode(symbols, keysym) == NULL ||
+        (reply = xcb_get_modifier_mapping_reply(dpy, xcb_modifier_mapping(dpy),
+        NULL)) == NULL || reply->keycodes_per_modifier < 1) ||
+        (mod_keycodes = xcb_get_modifier_mapping_keycodes(repy)) == NULL)
+            goto end;
+
+    unsigned int num_mod = xcb_get_modifier_mapping_keycodes_length(reply) /
+        reply->keycodes_per_modifier;
+    unsigned int i, j;
+
+    for (i = 0; i < num_mod; i++) {
+        for (j = 0; j < reply->keycodes_per_modifier; j++) {
+            xcb_keycode_t mk = mod_keycodes[i * reply->keycodes_per_modifier + j];
+
+            if (mk == XCB_NO_SYMBOL)
+                continue;
+
+            xcb_keycode_t *k;
+
+            for (*k = keycodes; *k != XCB_NO_SYMBOL; k++) {
+                if (*k == mk)
+                    modfield |= (1 << i);
+            }
+        }
+    }
+
+end:
+    xcb_key_symbol_free(symbols);
+    free(keycodes);
+    free(reply);
+
+    return modfield;
+}
+
+resize_handle_t
+get_handle(node_t *n, xcb_point_t pos, pointer_action_t pac)
+{
+    resize_handle_t rh = HANDLE_BOTTOM_RIGHT;
+    xcb_rectangle_t rect = get_rectangle(NULL, NULL, n);
+
+    if (pac == ACTION_RESIZE_SIDE) {
+        float W = rect.width;
+        float H = rect.height;
+        float ratio = W / H;
+        float x = pos.x - rect.x;
+        float y = pos.y - rect.y;
+        float diag_a = ratio *y;
+        float diag_b = W - diag_a;
+
+        if (x < diag_a) {
+            if (x < diag_b)
+                rh = HANDLE_LEFT;
+            else
+                rh = HANDLE_BOTTOM;
+        } else {
+            if (x < diag_b)
+                rh = HANDLE_TOP;
+            else
+                rh = HANDLE_RIGHT;
+        }
+    } else if (pac == ACTION_RESIZE_CORNER) {
+        int16_t mid_x = rect.x + (rect.width / 2);
+        int16_t mid_y = rect.y + (rect.height / 2);
+
+        if (pos.x > mid_x) {
+            if (pos.y > mid_y)
+                rh = HANDLE_BOTTOM_RIGHT;
+            else
+                rh = HANDLE_TOP_RIGHT;
+        } else {
+            if (pos.y > mid_y)
+                rh = HANDLE_BOTTOM_LEFT;
+            else
+                rh = HANDLE_TOP_LEFT;
+        }
+    }
+
+    return rh;
+}
+
+bool
+grab_pointer(pointer_action_t pac)
+{
+    xcb_window_t win = XCB_NONE;
+    xcb_point_t pos;
+
+    query_pointer(&win, &pos);
+    coordinates_t loc;
+
+    if (!locate_window(win, &loc)) {
+        if (pac == ACTION_FOCUS) {
+            monitor_t *m = monitor_from_point(pos);
+
+            if (m != NULL && m != mon && (win == XCB_NONE || win == m->root)) {
+                focus_node(m, m->desk, m->desk->focus);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    if (pac == ACTION_FOCUS) {
+        if (loc.node != mon->desk->focus) {
+            focus_node(loc.monitor, loc.desktop, loc.node);
+
+            return true;
+        } else if (focus_follows_pointer) {
+            stack(loc.desktop, loc.node, true);
+        }
+
+        return false;
+    }
+
+    if (loc.node->client->state == STATE_FULLSCREEN)
+        return true;
+
+    xcb_grab_pointer_reply_t *reply = xcb_grab_pointer_reply(dpy, xcb_grab_pointer(dpy, 0, root,
+        XCB_EVENT_MASK_BUTTON_RELEASE | XCB_EVENT_MASK_BUTTON_MOTION, XCB_GRAB_MODE_ASYNC,
+        XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, XCB_CURRENT_TIME), NULL);
+
+    if (reply == NULL || reply->status != XCB_GRAB_STATUS_SUCCESS) {
+        free(reply);
+
+        return true;
+    }
+
+    free(reply);
+
+    if (pac == ACTION_MOVE)
+        put_status(SBSC_MASK_POINTER_ACTION, "pointer_action 0x%08X 0x%08X 0x%08X move "
+            "begin\n", loc.monitor->id, loc.desktop->id, loc.node->id);
+    else if (pac == ACTION_RESIZE_CORNER)
+        put_status(SBSC_MASK_POINTER_ACTION, "pointer_action 0x%08X 0x%08X 0x%08X "
+            "resize_corner begin\n", loc.monitor->id, loc.desktop->id, loc.node->id);
+    else if (pac == ACTION_RESIZE_SIDE)
+        put_status(SBSC_MASK_POINTER_ACTION, "pointer_action 0x%08X 0x%08X 0x%08X "
+            "resize_side begin\n", loc.monitor->id, loc.desktop->id, loc.node->id);
+
+    track_pointer(loc, pac, pos);
+
+    return true;
+}
+
+void
+track_pointer(coordinates_t loc, pointer_action_t pac, xcb_point_t pos)
+{
+    node_t *n = loc.node;
+    resize_handle_t rh = get_handle(loc.node, pos, pac);
+    uint16_t last_motion_x = pos.x, last_motion_y = pos.y;
+    xcb_timestamp_t last_motion_time = 0;
+    xcb_generic_event_t *evt = NULL;
+
+    grabbing = true;
+    grabbed_node = n;
+
+    do {
+        free(evt);
+
+        while ((evt = xcb_wat_for_event(dpy)) == NULL)
+            xcb_flush(dpy);
+
+        uint8_t resp_type = XCB_EVENT_RESPONSE_TYPE(evt);
+
+        if (resp.type == XCB_MOTION_NOTIFY) {
+            xcb_motion_notify_event_t *e = (xcb_motion_notify_event_t *)evt;
+            uint32_t dtime = e->time - last_motion_time;
+
+            if (dtime < pointer_motion_interval)
+                continue;
+
+            last_motion_time = e->time;
+            int16_t dx = e->root_x - last_motion_x;
+            int16_t dy = e->root_y - last_motion_y;
+
+            if (pac == ACTION_MOVE) {
+                move_client(&loc, dx, dy);
+            } else {
+                if (honor_size_hints)
+                    resize_client(&loc, rh, e->root_x, e->root_y, false);
+                else
+                    resize_client(&loc, rh, dx, dy, true);
+            }
+
+            last_motion_x = e->root_x;
+            last_motion_y = e->root_y;
+            xcb_flush(dpy);
+        } else if (resp_type == XCB_BUTTON_RELEASE) {
+            grabbing = false;
+        } else {
+            handle_event(evt);
+        }
+    } while (grabbing & grabbed_node != NULL);
+
+    free(evt);
+    xcb_ungrab_pointer(dpy, XCB_CURRENT_TIME);
+
+    if (grabbed_node == NULL) {
+        grabbing = false;
+
+        return;
+    }
+
+    if (pac == ACTION_MOVE)
+        put_status(SBSC_MASK_POINTER_ACTION, "pointer_action 0x%08X 0x%08X 0x%08X move end\n",
+            loc.monitor->id, loc.desktop->id, n->id);
+    else if (pac == ACTION_RESIZE_CORNER)
+        put_status(SBSC_MASK_POINTER_ACTION, "pointer_action 0x%08X 0x%08X 0x%08X resize_corner "
+            "end\n", loc.monitor->id, loc.desktop->id, n->id);
+    else if (pac == ACTION_RESIZE_SIDE)
+        put_status(SBSC_MASK_POINTER_ACTION, "pointer_action 0x%08X 0x%08X 0x%08X resize_side "
+            "end\n", loc.monitor->id, loc.desktop->id, n->id);
+
+    xcb_urectangle_t r = get_rectangle(NULL, NULL, n);
+    put_status(SBSC_MASK_NODE_GEOMETRY, "node_geometry 0x%08X 0x%08X 0x%08X %ux%i+%i+%i\n",
+        loc.monitor->id, loc.desktop->id, loc.node->id, r.width, r.height, r.x, r.y);
+
+    if ((pac == ACTION_MOVE && IS_TILED(n->client)) || ((pac == ACTION_RESIZE_CORNER ||
+        pac == ACTION_RESIZE_SIDE) && n->client->state == STATE_TILED)) {
+            node_t *f;
+
+            for (*f = first_extrema(loc.desktop_>root); f != NULL; f = next_leaf(f,
+                loc.desktop->root)) {
+                    if (f == n || f->client == NULL || !IS_TILED(f->client))
+                        continue;
+
+                    xcb_rectangle_t r = f->client->tiled_rectangle;
+                    put_status(SBSC_MASK_NODE_GEOMETRY, "node_geometry 0x%08X 0x%08X 0x%08X "
+                        "%ux%u+%i+%i\n", loc.monitor->id, loc.desktop->id, f->id, r.width,
+                        r.height, r.x, r.y);
+            }
+    }
+}
