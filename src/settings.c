ae5213fdc59cb5a2243e70c13eb0fc60bcea7acb
Eric <londoed@comcast.net>
2021-02-26 17:02:35 -0500

    Added parsing functions

---
+++ src/settings.c
@@ -0,0 +1,5 @@
+/**
+ * Copyright (C) 2021, Eric Londo <londoed@comcast.net>, { src/settings.c }
+ * This software is distributed under the GNU General Public License Version 2.0.
+ * See the file LICENSE for details.
+**/
